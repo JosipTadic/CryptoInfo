cdc540d0e7a4cba91ffce8c4f0de1f7ca7a2bdc1
unknown <josiptadic96@hotmail.com>
2022-06-19 19:26:49 +0200

    useMemo removed from item page header

--- src/components/ItemPageHeader.tsx
+++ src/components/ItemPageHeader.tsx
@@ -1,4 +1,4 @@
-import React, { useMemo } from "react";
+import React from "react";
 import { Link } from "react-router-dom";
 import "bulma/css/bulma.min.css";
 import { IitemPageHeader } from "../types";
